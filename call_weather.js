c5c8a44738d758dc218e9befe8f9ecdb8258c73d
nyambura00 <winnienyambura14@gmail.com>
2020-10-23 17:25:06 +0300

    updated javascript

--- call_weather.js
+++ call_weather.js
@@ -1,10 +1,10 @@
-API_KEY = "0370bfb5ab6c164dfa9bfc236a549838";
-CURRENT_WEATHER_DATA = {};
+const API_KEY = "0370bfb5ab6c164dfa9bfc236a549838";
+const CURRENT_WEATHER_DATA = {};
 
-const cityElement = document.getElementById("city");
-const city = cityElement.innerHTML;
+const submitButton = document.getElementById("button");
+const city = document.getElementById("city").value;
 
-async function showWeatherFor(city) {
+submitButton.addEventListener("click", async function showWeatherFor(city) {
 	url_to_call = "http://api.openweathermap.org/data/2.5/weather?q="+city+"&appid="+API_KEY;
 	const response = await fetch(url_to_call)
 		.then((resp) => {
@@ -16,7 +16,7 @@ async function showWeatherFor(city) {
 			console.log(data)
 			CURRENT_WEATHER_DATA = data;
 		});
-}
-
+		return CURRENT_WEATHER_DATA
+})
 
 
